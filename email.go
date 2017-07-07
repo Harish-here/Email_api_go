e74304038e491b914f58dbfd5ee163e2f781e0ef
Harish Ravi <harishamudha@gmail.com>
2017-07-07 13:03:29 +0530

    working api with post data

--- email.go
+++ email.go
@@ -1,14 +1,32 @@
 package main
 import (
 	"net/http"
-	"log"
 	"fmt"
+	// "log"
+	// "encoding/json"
 )
+type userData struct {
+    id int
+	firstName string
+	secondName string
+	gender string
+}
+
+var err error
 func something(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w,"hello")
+	err := r.ParseForm()
+	if err != nil || len(r.Form) == 0 {
+		fmt.Fprintln(w,"No post data")
+		return
+	}
+	fmt.Fprintln(w,r.FormValue("harish"))
+}
+func sayhi(w http.ResponseWriter, r *http.Request){
+fmt.Fprintln(w,"hello")
 }
 
 func main(){
-	http.HandleFunc("/",something)
-	log.Fatal(http.ListenAndServe(":8000",nil))
+	http.HandleFunc("/people",something)
+	http.HandleFunc("/",sayhi)
+	http.ListenAndServe(":8080",nil)
 }
