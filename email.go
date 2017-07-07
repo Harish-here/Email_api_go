e75e4ae72ded7f2c2a7060fb05b12ebd7a7a5931
Harish Ravi <harishamudha@gmail.com>
2017-07-07 15:34:26 +0530

    working mysql

--- email.go
+++ email.go
@@ -2,8 +2,11 @@ package main
 import (
 	"net/http"
 	"fmt"
-	// "log"
+	"log"
 	// "encoding/json"
+	"encoding/json"
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
 )
 type userData struct {
     id int
@@ -11,18 +14,50 @@ type userData struct {
 	secondName string
 	gender string
 }
-
+var db *sql.DB
 var err error
+func setJSONAsHeader(w http.ResponseWriter){
+	w.Header().Set("Content-Type","application/json")
+}
 func something(w http.ResponseWriter, r *http.Request){
 	err := r.ParseForm()
 	if err != nil || len(r.Form) == 0 {
 		fmt.Fprintln(w,"No post data")
 		return
 	}
-	fmt.Fprintln(w,r.FormValue("harish"))
+	// fmt.Fprintln(w,r.Form)
+	setJSONAsHeader(w)
+	json.NewEncoder(w).Encode(r.Form)
 }
 func sayhi(w http.ResponseWriter, r *http.Request){
-fmt.Fprintln(w,"hello")
+	r.ParseForm()
+	var ser string
+	if r.FormValue("id") != ""{
+		 ser = r.FormValue("id")
+	}
+	var (
+		id string
+		firstName string
+		secondName string
+		gender string
+	)
+db, err = sql.Open("mysql", "root:@/user_db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	rows,err := db.Query("SELECT * FROM user_tb WHERE id ="+ser)
+	if err != nil{
+		panic(err)
+	}
+	for rows.Next(){
+		err := rows.Scan(&id,&firstName,&secondName,&gender) 
+		if err != nil{
+			panic(err)
+		}
+		fmt.Fprintln(w,id + " " + firstName +" "+secondName+" "+gender)
+	}
+	
 }
 
 func main(){
