75d8f79b3b6f04c654a347d73a2baea5a75072ec
Harish Ravi <harishamudha@gmail.com>
2017-07-10 12:15:20 +0530

    contact email working
    contact email is working and tested

--- email.go
+++ email.go
@@ -63,45 +63,73 @@ func checkerr(err error){
 			log.Fatal(err)
 		}
 }
-type person struct{
-	Username string
-}
-func getTemplate(w http.ResponseWriter, r *http.Request){
+
+func getTemplates(w http.ResponseWriter, r *http.Request){
 	t,_ := template.ParseFiles("confirm.html")//parsing the file to template
 	buf := new(bytes.Buffer) //contanier to hold the string of html code
-	err := t.Execute(buf,person{Username:"harish"})
+	err := t.Execute(buf,person{customerName:"Madhan",phoneNumber:"9791528548",customerEmail:"madhan@infonixweblab.com",message:"hi this is a sample"})
 	checkerr(err)
 	fmt.Fprintln(w,buf)
 
 }
 
-func sendMail(w http.ResponseWriter, r *http.Request){
+func sendMail(w http.ResponseWriter,from string,to string,subject string,body string){
 	m := gomail.NewMessage()
-m.SetHeader("From", "harish@infonixweblab.com")
-m.SetHeader("To", "justinsylas@infonixweblab.com")
-m.SetHeader("Subject", "Hello this is a test mail! from go API")
-	t,_ := template.ParseFiles("confirm.html")
+	m.SetHeader("From",from)
+	m.SetHeader("To",to)
+	m.SetHeader("subject",subject)
+	m.SetBody("text/html",body)
+	d := gomail.NewDialer("smtp.zoho.com", 587, "harish@infonixweblab.com", "harish123")
+	if err := d.DialAndSend(m); err != nil {
+   		 panic(err)
+		}
+			fmt.Fprint(w,"mail sent")
+		
+}
+
+
+
+func getTemplate(file string,data templatedata) string {
+	t,_ := template.ParseFiles(file)
 	buf := new(bytes.Buffer)
-	if err = t.Execute(buf,person{Username:"harish"});err != nil {
+	if err = t.Execute(buf,data);err != nil {
 		panic(err)
 	}
- body := buf.String()
-m.SetBody("text/html", body)
-
-
-d := gomail.NewDialer("smtp.zoho.com", 587, "harish@infonixweblab.com", "harish123")
-
-// Send the email to the target thru host
-if err := d.DialAndSend(m); err != nil {
-    panic(err)
-}else{
-	fmt.Fprint(w,"mail sent")
+	
+		return buf.String()
+	
 }
+type person struct{
+	customerName string
+	phoneNumber string
+	customerEmail string
+	message string
+}
+ 
+type templatedata struct{
+	CustomerName string
+	PhoneNumber string
+	CustomerEmail string
+	Message string
+}
+func sendMails(w http.ResponseWriter, r *http.Request){
+d:= templatedata{
+	CustomerName : "harsih",
+	PhoneNumber :"8870072364",
+	CustomerEmail : " harish@infonixweblab.com",
+	Message : " this is a sample message",
+}
+
+body := getTemplate("checkout.html",d)
+sendMail(w,"harish@infonixweblab.com","madhan@infonixweblab.com","Booking Confirmation",body)
 }
 func main(){
+
+
+
 	http.HandleFunc("/people",something)
 	http.HandleFunc("/",sayhi)
-	http.HandleFunc("/template",getTemplate)
-	http.HandleFunc("/sendmail",sendMail)
+	http.HandleFunc("/template",getTemplates)
+	http.HandleFunc("/sendmail",sendMails)
 	log.Fatal(http.ListenAndServe(":3306",nil))
 }
